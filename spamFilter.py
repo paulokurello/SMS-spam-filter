7d10c8f586878fae1956f89af1e953caba5e8052
gorecka <gosiula0509@wp.pl>
2020-01-27 15:40:27 +0100

    Writing results to file

--- spamFilter.py
+++ spamFilter.py
@@ -1,3 +1,4 @@
+import numpy
 import pandas
 import string
 import math
@@ -54,12 +55,11 @@ class Classifier:
         self.tfidfSumHam = 0
         self.tfidfSumSpam = 0
 
-    def calcParametersBOW(self):
+    def calcParametersBOW(self, lowerCase = False, stemWords=False, rmStopWords=False):
         for i in range(self.messageCount):
-            label = self.labels[i]
-            words = preprocessMessage(self.messages[i])
+            words = preprocessMessage(self.messages.iloc[i], lowerCase, stemWords, rmStopWords)
             for word in words:
-                if label == 1:
+                if self.labels.iloc[i]:
                     self.wordsSpamCount[word] = self.wordsSpamCount.get(word, 0) + 1
                     self.spamWordsCount += 1
                 else:
@@ -79,8 +79,8 @@ class Classifier:
 
     def calcParametersTFIDF(self):
         for i in range(self.messageCount):
-            label = self.labels[i]
-            words = preprocessMessage(self.messages[i])
+            label = self.labels.iloc[i]
+            words = preprocessMessage(self.messages.iloc[i])
             for word in words:
                 filter(lambda a: a != word, words)
                 if label == 1:
@@ -110,9 +110,11 @@ class Classifier:
         self.tmpMsg = testingSet['message']
         self.tmpLab = testingSet['value']
         self.tmpcount = self.tmpMsg.size
+        self.correct = 0
+        predictionList = list()
         for i in range(self.tmpcount):
-            label = self.tmpLab[i]
-            words = preprocessMessage(self.tmpMsg[i])
+            label = self.tmpLab.iloc[i]
+            words = preprocessMessage(self.tmpMsg.iloc[i])
             SpamProb = self.probSpam
             HamProb = self.probHam
             for word in words:
@@ -129,10 +131,24 @@ class Classifier:
                 prediction = 0
             else:
                 prediction = 1
-            if prediction == label:
-                print('prediction successful', label, ' ', prediction)
-            else:
-                print('prediction fail', label, ' ', prediction)
+            predictionList.append(prediction)
+        return predictionList
+
+
+def statistics(labels, predictions):
+    truePos, trueNeg, falsePos, falseNeg = 0, 0, 0, 0
+    for i in range(len(labels)):
+        truePos += int(labels.iloc[i] == 1 and predictions[i] == 1)
+        trueNeg += int(labels.iloc[i] == 0 and predictions[i] == 0)
+        falsePos += int(labels.iloc[i] == 0 and predictions[i] == 1)
+        falseNeg += int(labels.iloc[i] == 1 and predictions[i] == 0)
+    precision = truePos / (truePos + falsePos)
+    recall = truePos / (truePos + falseNeg)
+    Fscore = 2 * precision * recall / (precision + recall)
+    accuracy = (truePos + trueNeg) / (truePos + trueNeg + falsePos + falseNeg)
+    result = "Precision: ," + str(precision) + ", Recall: ," + str(recall) + ", F-score: ," + str(Fscore) + ", Accuracy: ," + str(accuracy) + ', \n'
+    plik.write(result)
+
 
 data = readDataSet('spam.csv', ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'])
 data.rename({'v1': 'value', 'v2': 'message'}, axis=1, inplace=True)
@@ -140,16 +156,23 @@ print(data.head())
 print(data['value'].value_counts())
 data['value'] = data['value'].map({'ham': 0, 'spam': 1})
 print(data.head())
-
-test = Classifier(data)
-test.calcParametersBOW()
-print(data['value'][250])
-print(data['message'][250])
-
-test.TestMessages(data)
+plik = open('BOWsam', 'w')
 
 
+for k in range(5):
+    trainIndex, testIndex = list(), list()
+    for i in range(data.shape[0]):
+        if numpy.random.uniform(0, 1) < 0.75:
+            trainIndex += [i]
+        else:
+            testIndex += [i]
+    trainData = data.loc[trainIndex]
+    testData = data.loc[testIndex]
 
+    test = Classifier(trainData)
+    test.calcParametersBOW(False, False, False)
+    results = list()
+    results = test.TestMessages(testData)
+    statistics(testData['value'], results)
 
-
-
+plik.close()
